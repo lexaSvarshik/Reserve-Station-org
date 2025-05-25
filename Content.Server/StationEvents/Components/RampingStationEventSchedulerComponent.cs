c8c1551551f57c54189314233702aeea8ecd95a6
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-05-25 11:17:17 +0000

    chore: Automatically update REUSE headers

--- Content.Server/StationEvents/Components/RampingStationEventSchedulerComponent.cs
+++ Content.Server/StationEvents/Components/RampingStationEventSchedulerComponent.cs
@@ -4,6 +4,8 @@
 // SPDX-FileCopyrightText: 2024 Aidenkrz <aiden@djkraz.com>
 // SPDX-FileCopyrightText: 2024 IProduceWidgets <107586145+IProduceWidgets@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
