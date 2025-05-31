cc13cf89747c8e88d54e6d1b89533761b76d1403
Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
2025-05-31 17:51:04 +0500

    map changes - new markers

---
+++ Content.Server/_CorvaxNext/BattleRoyale/Rules/Components/BattleRoyaleSpawnMarkerComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameObjects;
+
+namespace Content.Server._CorvaxNext.BattleRoyale;
+
+/// <summary>
+/// Marks a spawn point for players in Battle Royale mode.
+/// </summary>
+[RegisterComponent]
+public sealed partial class BattleRoyaleSpawnMarkerComponent : Component
+{
+}
