2c047a34111b40d4431bce93f1fdd205d12f8b4b
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-07-13 18:05:40 +0000

    chore: Automatically update REUSE headers

--- Content.Server/Administration/PlayerLocator.cs
+++ Content.Server/Administration/PlayerLocator.cs
@@ -8,6 +8,7 @@
 // SPDX-FileCopyrightText: 2024 username <113782077+whateverusername0@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2024 whateverusername0 <whateveremail>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 ReserveBot <211949879+ReserveBot@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
 // SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
