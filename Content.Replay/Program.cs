73c0554e2a7361cbdcd5792166282529e6c8328f
ReserveBot <211949879+ReserveBot@users.noreply.github.com>
2025-07-13 18:04:15 +0000

    chore: Automatically update REUSE headers

--- Content.Replay/Program.cs
+++ Content.Replay/Program.cs
@@ -1,5 +1,8 @@
 // SPDX-FileCopyrightText: 2023 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
 // SPDX-FileCopyrightText: 2025 Aiden <28298836+Aidenkrz@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 SX-7 <sn1.test.preria.2002@gmail.com>
+// SPDX-FileCopyrightText: 2025 Svarshik <96281939+lexaSvarshik@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 nazrin <tikufaev@outlook.com>
 //
 // SPDX-License-Identifier: MIT
 
